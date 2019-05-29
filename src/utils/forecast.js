70e9f3ea8857f0f837c79a12643cf91b9189ae73
Daniel Carrera <dcarrera@danielshealth.com>
2019-05-29 18:55:23 -0500

    Add high and low to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,10 +10,12 @@ const url ='https://api.darksky.net/forecast/eb414d590a0623fe46221a167b746902/'+
     } else if (body.error) {
       callback(body.error, undefined)
     }else {
+      
     callback(undefined,body.daily.data[0].summary + 
       ' It is currently '+body.currently.temperature+' degrees out. '+ 
       'There is ' + body.currently.precipProbability * 100 + '% chance of rain.'  + 
-    'The windspeed is '+ body.currently.windSpeed)
+    ' The high today is ' + body.daily.data[0].temperatureHigh+ ' with a low of ' + 
+    body.daily.data[0].temperatureLow+'. The windspeed is '+ body.currently.windSpeed) + '.'
      
     }
 
